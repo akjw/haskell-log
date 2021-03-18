18bb9222f9b2e0a8f5c0ad6b4f6d7e2170db422f
akjw <aseykoh@gmail.com>
2021-03-18 17:08:27 +0800

    add either ex

--- 18_monad/18.hs
+++ 18_monad/18.hs
@@ -142,7 +142,8 @@ mkSphericalCow'' :: String
   -> Maybe Cow
 mkSphericalCow'' name' age' weight' =
   noEmpty name' >>= -- Monadic instance of Maybe: a Nothing return value will cause >>= to drop rest of computation
-  \nammy ->
+  -- with noEmpty "Bess", "Bess" is passed as a string instead of Just "Bess", since >>= binds over monadic value a, not m a
+  \nammy -> 
   noNegative age' >>=
   \agey ->
   noNegative weight' >>=
@@ -155,3 +156,66 @@ mkSphericalCow'' name' age' weight' =
 -- i.e. lifting functions that are also Just or Nothing over Maybe values.
 -- With the Maybe Monad, computations contributing to the final
 -- result can choose to return Nothing based on previous computations.
+
+-- Either
+
+-- years ago
+type Founded = Int
+-- number of programmers
+type Coders = Int
+
+data SoftwareShop =
+  Shop {
+  founded :: Founded
+  , programmers :: Coders
+  } deriving (Eq, Show)
+
+data FoundedError =
+  NegativeYears Founded
+  | TooManyYears Founded
+  | NegativeCoders Coders
+  | TooManyCoders Coders
+  | TooManyCodersForYears Founded Coders
+  deriving (Eq, Show)
+
+validateFounded :: Int -> Either FoundedError Founded
+validateFounded n
+  | n < 0 = Left $ NegativeYears n
+  | n > 500 = Left $ TooManyYears n
+  | otherwise = Right n
+
+validateCoders :: Int -> Either FoundedError Coders
+validateCoders n
+  | n < 0 = Left $ NegativeCoders n
+  | n > 5000 = Left $ TooManyCoders n
+  | otherwise = Right n
+
+mkSoftware :: Int -> Int -> Either FoundedError SoftwareShop
+mkSoftware years coders = do
+  founded <- validateFounded years 
+  -- since values can be dependent in Monad, Either always short-circuits on first point of failure
+  programmers <- validateCoders coders
+  if programmers > div founded 10
+    then Left $ TooManyCodersForYears founded programmers
+    else Right $ Shop founded programmers
+
+-- Short Exercise: Either Monad
+data Sum a b =
+    First a
+  | Second b
+  deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap _ (First x) = First x
+  fmap f (Second y) = Second (f y)
+
+instance Applicative (Sum a) where
+  pure = Second
+  (First x)  <*>  _         = First x
+  _          <*> (First x)  = First x
+  (Second f) <*> (Second x) = Second (f x)
+
+instance Monad (Sum a) where
+  return = pure
+  (First x)  >>= _    = First x
+  (Second y) >>= f    = f y
